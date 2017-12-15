734c178d9a9d1a11200952f4bd448a6b85caa098
Paul Cadman <git@paulcadman.io>
2017-12-15 10:23:53 +0000

    Initial commit of project

---
+++ app/src/main/java/io/paulcadman/zecamp/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.paulcadman.zecamp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
